200fcb082c7b003886c37f08f347f48aac13c74c
Steffen <kelzenberg@users.noreply.github.com>
2017-11-10 18:34:48 +0100

    added IPversion and prefix display

--- src/com/main/Networking.java
+++ src/com/main/Networking.java
@@ -16,14 +16,21 @@ public class Networking {
     private static void displayInterfaceInformation(NetworkInterface netintf) throws SocketException {
         out.printf("Display name: %s\n", netintf.getDisplayName());
         out.printf("Name: %s\n", netintf.getName());
-        Enumeration<InetAddress> inetAddresses = netintf.getInetAddresses();
-        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
-            out.printf("InetAddress: %s\n", inetAddress);
+        List<InterfaceAddress> intfAdresses = netintf.getInterfaceAddresses();
+        for (InterfaceAddress intfAdress : intfAdresses) {
+            out.printf("| InetAddress: %s\n", intfAdress.getAddress());
+            out.printf("| IP-Version: %s\n", getIPVersion(intfAdress));
+            out.printf("| Prefix: %s\n", intfAdress.getNetworkPrefixLength());
+            out.printf("\n");
         }
         out.printf("\n");
     }
 
-    private String getIPVersion(){
-        return null;
+    private static String getIPVersion(InterfaceAddress intfAdress){
+        if (!intfAdress.getAddress().toString().contains(".")){
+            return "IPv6";
+        } else {
+            return "IPv4";
+        }
     }
 }
