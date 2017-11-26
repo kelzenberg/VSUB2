59f68448d023391b99e3ae4e5b17ce23f3bfa391
Steffen <kelzenberg@users.noreply.github.com>
2017-11-26 18:38:07 +0100

    added comments

--- src/com/main/Broadcaster.java
+++ src/com/main/Broadcaster.java
@@ -11,23 +11,28 @@ public class Broadcaster extends TimerTask {
     DatagramSocket socket;
     
     public Broadcaster(ArrayList<InetAddress> list) {
+        // create a new Socket
         try {
             socket = new DatagramSocket();
         } catch(Exception e) {
-            
+            System.out.println("Socket could not be created: " + e);
         }  
+        // set Port and Broadcast-Message
         port = 9876;
         message = ("Dieser Server wurde von Jannis Rieger, Steffen Ansorge und Nicolai Brandt implementiert und stellt die Fibonacci-Funktion als Dienst bereit. Um den Dienst zu nutzen, senden Sie eine Nachricht an Port 65535 auf diesem Server. Das Format der Nachricht sollte folgendermaßen aussehen: ASCII encoded String: 'get;[nummer]' Um die Fibonaccizahl der Zahl 5 zu erhalten muss z.B. 'get;5' übermittelt werden.").getBytes();
-        
+        // save broadcastAddresses
         broadcastAddresses = list;
     }
     
     public void run() {
         System.out.println("[BROADCASTER]: Broadcast running.");
+        // for every Broadcast-Address in reach...
         for (InetAddress curInstance: broadcastAddresses) {
             System.out.println("[BROADCASTER]: " + curInstance);
+            // ...create a Packet, fill it with the Message etc...
             DatagramPacket packet = new DatagramPacket(message, message.length, curInstance, port);
             try {
+                // ...and send it
                 socket.send(packet);
             } catch(Exception e) {
                 System.out.println("[BROADCASTER]: An error occoured while sending Broadcast.");
